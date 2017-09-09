ca36e26473e86cbeb56346873c26fe1fa5a9a791
Yogesh Ojha <yogesh.ojha11@gmail.com>
2017-09-10 01:15:16 +0530

    Added Question

--- All Vowels.cpp
+++ All Vowels.cpp
@@ -1,3 +1,28 @@
+/*
+Vowels are very essential characters to form any meaningful word in English dictionary. There are 5 vowels in English language - a, e, i, o u. You are given a randoms string containing only lowercase letters and you need to find if the string contains ALL the vowels.
+
+Input:
+
+FIrst line contains N , the size of the string.
+Second line contains the letters (only lowercase).
+
+Output:
+
+Print "YES" (without the quotes) if all vowels are found in the string, "NO" (without the quotes) otherwise.
+
+Constraints:
+
+The size of the string will not be greater than 10,000
+1 ≤ N ≤ 10000
+
+SAMPLE INPUT 
+8
+atuongih
+SAMPLE OUTPUT 
+NO
+
+URL: https://www.hackerearth.com/practice/data-structures/hash-tables/basics-of-hash-tables/practice-problems/algorithm/all-vowels-2/
+*/
 #include <bits/stdc++.h>
 using namespace std;
 string vowel(string s)
