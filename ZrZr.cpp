d93b5822c46800596b74e9f4e25bbfbf0aa31a01
Yogesh Ojha <yogesh.ojha11@gmail.com>
2017-09-10 01:07:49 +0530

    Question Added, Partial Accept Removed, Pass param as long int

--- ZrZr.cpp
+++ ZrZr.cpp
@@ -1,6 +1,32 @@
+/*
+You are given a number N. How many zeroes does N! end on?
+
+Input
+The first line contains one integer T - number of test cases. The following T lines contain one integer each - N.
+
+Output
+For each test case output one integer per line - the answer for this question.
+
+Constraints
+
+T <= 1000
+0 <= N <= 1011
+SAMPLE INPUT 
+3
+9
+11
+20
+SAMPLE OUTPUT 
+1
+2
+4
+
+Question: https://www.hackerearth.com/practice/math/number-theory/basic-number-theory-2/practice-problems/algorithm/zrzr/
+*/
 #include <bits/stdc++.h>
 using namespace std;
-long int trail(int n)
+//the earlier solution was partially accepted because I forget to pass n as long int
+long int trail(long int n)
 {
     long int cnt = 0;
     while(n>0)
@@ -13,7 +39,7 @@ long int trail(int n)
 int main()
 {
     int tc;
-    double n;
+    long long n;
     cin>>tc;
     while(tc--)
     {
