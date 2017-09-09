946e2ecb59d23e7d0d558ad7a4fcb6130f6662ef
Yogesh Ojha <yogesh.ojha11@gmail.com>
2017-09-09 23:27:18 +0530

    Added Question

--- Trailing_Zeros.cpp
+++ Trailing_Zeros.cpp
@@ -1,3 +1,28 @@
+/*
+Given a number find the number of trailing zeroes in its factorial.
+
+Input Format
+
+A single integer - N
+
+Output Format
+
+Print a single integer which is the number of trailing zeroes.
+
+Input Constraints
+
+1 <= N <= 1000
+
+Problem Setter: Practo Tech Team
+
+SAMPLE INPUT 
+10
+SAMPLE OUTPUT 
+2
+Explanation
+10! = 3628800 has 2 zeros in the end.
+URL: https://www.hackerearth.com/problem/algorithm/trailing-zeros/
+*/
 #include <iostream>
 using namespace std;
 int trail(int n)
