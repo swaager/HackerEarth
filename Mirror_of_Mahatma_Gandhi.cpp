8e3fe991eef9241c5e4a5cc1b93d82714ce9f97c
Yogesh Ojha <yogesh.ojha11@gmail.com>
2017-09-15 00:47:41 +0530

    Question Added

--- Mirror_of_Mahatma_Gandhi.cpp
+++ Mirror_of_Mahatma_Gandhi.cpp
@@ -1,3 +1,22 @@
+/*
+On the way to Dandi March, Gandhijee carried a mirror with himself. 
+When he reached Dandi, he decided to play a game with the tired people to give them some strength. 
+At each turn of the game he pointed out a person and told him to say a number N(possibly huge) of his choice. 
+The number was called lucky if that equals it's mirror image.
+
+Input: 
+First line contains number of test cases T. Each test case contains a single integer N.
+Output: 
+For each test case print "YES" if the number was lucky else print "NO" (quotes for clarity) in one line.
+
+Constraints:
+1 ≤ T ≤ 100
+0 ≤ N ≤ 10100
+
+Image for Sample Test Cases :
+Refer Question: 
+https://www.hackerearth.com/practice/algorithms/string-algorithm/basics-of-string-manipulation/practice-problems/algorithm/mirror-of-mahatma-gandhi/
+*/
 #include <bits/stdc++.h>
 using namespace std;
 void dandi(string s)
