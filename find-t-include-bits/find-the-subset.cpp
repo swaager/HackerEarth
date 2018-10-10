79aaac2523edee6e614ce80357b45c0ee1ca3092
kishanradia15 <201601236@daiict.ac.in>
2018-10-10 14:44:52 +0530

    Added my solution for problem named "Find the subset".

---
+++ find-t-include-bits/find-the-subset.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    ll n,m,d;
+    cin>>n>>m>>d;
+    ll b[m],a[m],i;
+    for(i=0;i<m;i++){
+        cin>>a[i];
+        b[i]=a[i];
+        
+    }
+    sort(b,b+m);
+    set<ll> s;
+    for(i=0;i<n;i++)s.insert(b[i]);
+    for(i=0;i<m;i++){
+        if(s.find(a[i])!=s.end()){
+            cout<<a[i]<<" ";
+        }
+    }
+    return 0;
+}
